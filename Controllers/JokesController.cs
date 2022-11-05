b92c0275515de41e5850389b43d429ed13990018
lalarcon-py <74345096+lalarcon-py@users.noreply.github.com>
2022-11-04 21:15:57 -0700

    Included Authorization for adding, editing, and deleting objects

--- Controllers/JokesController.cs
+++ Controllers/JokesController.cs
@@ -66,6 +66,7 @@ namespace JokesApp.Controllers
         // POST: Jokes/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,JokeQuestion,JokeAnswer")] Joke joke)
